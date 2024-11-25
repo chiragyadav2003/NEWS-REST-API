3d2379228632dffb6aafe41f8aacdd7afc8c6f56
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-11-26 01:45:42 +0530

    adding helmet and cors, also added unknown route issue and general error handling middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import "dotenv/config";
 import fileUpload from "express-fileupload";
+import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -8,6 +10,8 @@ const PORT = process.env.PORT || 8000;
 // * Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cors());
 app.use(express.static("public")); //statically serve files from 'public' directory
 app.use(fileUpload());
 
@@ -21,6 +25,23 @@ app.get("/", (req, res) => {
 // * Import routes
 import { apiRouter } from "./routes/api.js";
 app.use("/api", apiRouter);
+
+// * 404 route for unmatched paths
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found.",
+  });
+});
+
+// * general error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "An unexpected error occurred.",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port : ${PORT}`);
